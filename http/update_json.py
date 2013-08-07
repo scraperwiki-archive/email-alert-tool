63f8b8a5912b7904fa6d238170d031b02c128844
Sean Duffy <seanduffy@live.com>
2013-08-07 14:16:18 +0000

    Fixed some paths in update_json.py and some changes to form.js

--- http/update_json.py
+++ http/update_json.py
@@ -7,8 +7,9 @@ import argparse
 import scraperwiki
 
 def open_json_and_convert_to_dictionary():
-    if os.path.isfile('../metadata.json') == True:
-        json_string = open('../metadata.json').read()
+    print os.getcwd()
+    if os.path.isfile(os.getcwd() +  '/metadata.json') == True:
+        json_string = open(os.getcwd() + '/metadata.json').read()
         json_out = json.loads(json_string)
         return json_out
     else: 
@@ -18,13 +19,13 @@ def open_json_and_convert_to_dictionary():
 def update_json_file():
     config = open_json_and_convert_to_dictionary()
     config['recipient'] = sys.argv[1]
+    print sys.argv[1], sys.argv[2], sys.argv[3]
     scraperwiki.sqlite.save_var("recipient", sys.argv[1])
     config['url'] = sys.argv[2]
     scraperwiki.sqlite.save_var("url", sys.argv[2])
     config['tablename'] = sys.argv[3]
     scraperwiki.sqlite.save_var("tablename", sys.argv[3])
-    json.dump(config, open("metadata1.json", "w"))
-
+    json.dump(config, open("metadata.json", "w"))
 
 def main():
     update_json_file()
@@ -33,5 +34,5 @@ if __name__ == '__main__':
    if len(sys.argv) == 4:
        main()
    else: 
-       "Not enough arguments."
+       print "Not enough arguments."
        exit() 
