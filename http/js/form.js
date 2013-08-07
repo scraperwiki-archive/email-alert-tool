2177a702901b4573dbb25274a55ae947195f1662
Matthew Hughes <me@matthewhughes.co.uk>
2013-08-07 10:09:15 +0000

    Added form handling code

--- http/js/form.js
+++ http/js/form.js
@@ -1,3 +1,35 @@
-var email = $('#email').val()
-var tableName = $('#tablename').val()
+$(function() {
 
+    var execSuccess = function(execOutput) {
+        var datasetUrl = '/dataset/' + scraperwiki.box
+        scraperwiki.tool.redirect(datasetUrl)
+    }
+
+    var populateForm = function() {
+        var email = scraperwiki.sql.("select value_blob from swvariables where name='recipient';", function(data, textStatus, jqXHR) {
+            console.log("Success")
+        },
+        function(jqXHR, textStatus, errorThrown) {
+            console.log("Error! " + jqXHR + textStatus + errorThrown)   
+        })
+ 
+        var tableName = scraperwiki.sql.("select value_blob from swvariables where name='tablename';", function(data, textStatus, jqXHR) {
+            console.log("Success")
+        },
+        function(jqXHR, textStatus, errorThrown) {
+            console.log("Error! " + jqXHR + textStatus + errorThrown)   
+        })
+
+        $('#email').val(email);
+        $('#tablename').val(tableName);
+
+    }
+    
+    $('#submit').on('click', function() {
+        var email = $('#email').val()
+        var tableName = $('#tablename').val()
+        var endpointUrl = scraperwiki.readSettings().target.url + '/sqlite?q=select count(*) from ' + tableName + ';'
+
+        scraperwiki.exec('../update_json.py ' + email + ' ' + endpointUrl + ' '  + tableName, execSuccess)
+    })
+})
