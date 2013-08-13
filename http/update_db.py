caf7be588bad38b2e3f153d6c87ef88502e8b4e0
Sean Duffy <seanduffy@live.com>
2013-08-13 09:16:37 +0000

    Changed python to use sqlite and not json

---
+++ http/update_db.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import sys
+import scraperwiki
+
+def update_db():
+    scraperwiki.sqlite.save_var("recipient", sys.argv[1])
+    scraperwiki.sqlite.save_var("url", sys.argv[2])
+    scraperwiki.sqlite.save_var("tablename", sys.argv[3])
+
+def main():
+    update_db()
+
+if __name__ == '__main__':
+   if len(sys.argv) == 4:
+       print len(sys.argv)
+       main()
+   else: 
+       print "Not enough arguments: email, url to SQL, tablename"
+       exit() 
